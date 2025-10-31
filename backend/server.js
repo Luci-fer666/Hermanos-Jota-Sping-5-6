ac6cf107ea3aef625799c3ac5ea9ed14f1b0ec53
Lola <lolafioramantiuni@gmail.com>
2025-10-31 13:06:58 -0300

    Agregados endpoints de get

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,48 @@ app.get('/', (req, res) => {
   res.json({ message: '¡Bienvenido al servidor de Mueblería Jota!' });
 });
 
+// END POINT 1: Obtener productos
+app.get('/api/productos', async (req, res, next) => {
+  try {
+    const productos = await Producto.find({});
+    res.status(200).json(productos);
+  } catch (error) {
+    consola.error('Error al obtener los productos: ', error.message);
+    next(error);
+  }
+})
+
+// END POINT 2: Obtener un producto con su ID
+app.get('/api/productos/:id', async (req, res, next) => {
+  try {
+    const productoId = req.params.id // id que crea automaticamente la base de datos
+    const producto = await Producto.findById(productoId);
+    
+    // si no se encuentra el producto
+    if (!producto) {
+      const error = new Error('Producto no encontrado');
+      error.status = 404;
+      return next(error); // termina en el manejador de errores
+    }
+    res.status(200).json(usuario);
+  } catch (error) {
+    console.error('Error al buscar producto por su ID: ', error.message);
+    error.status = 400;
+    next(error);
+  }
+})
+
+// -----------------
+// MANEJO DE ERRORES
+
 // Middleware para rutas no encontradas (404)
 app.use(notFound);
 
 // Middleware de manejo de errores
 app.use(errorHandlerServer);
+
+
+// ----------------
 
 // Levantar servidor
 app.listen(PORT, () => {
